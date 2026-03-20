19433a5a9b9e96828e21ae679cc3ad8cf7233693
jianyun-wu <126971979+jianyun-wu@users.noreply.github.com>
2026-03-20 20:23:26 +0800

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  author: "Admin",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "Wise",
